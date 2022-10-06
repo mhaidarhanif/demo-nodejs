ab76fa8c91cf49492b3003947aba6cf29d81eed2
M Haidar Hanif <git@mhaidarhanif.com>
2022-10-07 00:11:30 +0700

    feat: demo format updatedAt with dayjs

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const timeElement = document.getElementById("time");
+
+dayjs.extend(window.dayjs_plugin_relativeTime);
+
+const updatedAt = new Date(2022, 5, 20);
+const updatedAtText = dayjs(updatedAt).fromNow();
+
+timeElement.innerText = updatedAtText;
